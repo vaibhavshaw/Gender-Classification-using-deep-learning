4d11b031aa32742c86afd03e3066d5fd2fbb5d34
Vaibhav Shaw <35895188+vaibhavshaw@users.noreply.github.com>
2019-05-11 20:26:55 +0530

    python3

--- Gender_Classification.py
+++ Gender_Classification.py
@@ -81,18 +81,21 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
+batch_size = 64
+
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
-                                                 batch_size = 32,
+                                                 batch_size = batch_size,
                                                  class_mode = 'binary')
+
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
+                                            batch_size = batch_size,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 10,
+                         steps_per_epoch = 4000/batch_size,
+                         epochs = 50,
                          validation_data = test_set,
-                         validation_steps = 2000)
+                         validation_steps = 1000/batch_size)
